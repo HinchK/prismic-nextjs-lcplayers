016acd1b863b060fef47dc409db2156e10ec87a0
hinchk <kasey.hinchman@gmail.com>
2024-03-11 19:15:02 -0700

    use next/script to add preview links to all pages

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,26 +1,33 @@
-import { Header } from "./Header";
-import { Footer } from "./Footer";
+import {Header} from "./Header";
+import {Footer} from "./Footer";
+import Script from 'next/script'
+
+export default function PreviewLink() {
+    return (
+        <>
+            <Script src="https://static.cdn.prismic.io/prismic.js?new=true&repo=lcplayers"/>
+        </>
+    )
+}
 
 export function Layout({
-  navigation,
-  settings,
-  withHeaderDivider,
-  withProfile,
-  withSignUpForm,
-  children,
-}) {
-  return (
-      <div className="text-slate-700">
-          <Header
-              withProfile={withProfile}
-              withDivider={withHeaderDivider}
-              navigation={navigation}
-              settings={settings}
-          />
-          <main>{children}</main>
-          <Footer withSignUpForm={withSignUpForm} settings={settings}/>
-
-          <script async defer src="https://static.cdn.prismic.io/prismic.js?new=true&repo=lcplayers"></script>
-      </div>
-  );
+                           navigation,
+                           settings,
+                           withHeaderDivider,
+                           withProfile,
+                           withSignUpForm,
+                           children,
+                       }) {
+    return (
+        <div className="text-slate-700">
+            <Header
+                withProfile={withProfile}
+                withDivider={withHeaderDivider}
+                navigation={navigation}
+                settings={settings}
+            />
+            <main>{children}</main>
+            <Footer withSignUpForm={withSignUpForm} settings={settings}/>
+        </div>
+    );
 }
